6e5c5f984358ef19ee6dac95be84a2152e1845ba
divv919 <divyanshsoni919@gmail.com>
2025-11-14 14:04:48 +0530

    fix: add favicon, fix indicator

--- app/components/Indicator.tsx
+++ app/components/Indicator.tsx
@@ -8,18 +8,15 @@ export default function Indicator() {
         animate={{ top: "50%" }}
         whileHover={{ top: 0 }}
         whileTap={{ top: 0 }}
-        className="shadow-[inset_0_0_10px_rgba(255,255,255,0.2)] h-13 w-30 rounded-md rounded-b-none  bg-neutral-900/60  border-t border-neutral-800  absolute left-1/2 bottom-0 -translate-x-1/2 flex flex-col gap-1 p-[6px] text-xs select-none cursor-pointer "
+        className="shadow-[inset_0_0_10px_rgba(255,255,255,0.2)] h-13 w-30 rounded-md rounded-b-none  bg-neutral-900/60  border-t border-neutral-800  absolute left-1/2 bottom-0 -translate-x-1/2 flex flex-col gap-1 py-[6px]  text-xs select-none r "
       >
         <div className="flex gap-2 justify-center items-center text-neutral-500 tracking-tight ">
-          <span className="size-1 rounded-full bg-green-500"></span>
+          <span className="size-1 rounded-full bg-green-500 animate-pulse shadow-2xl "></span>
           Open to work
         </div>
         <div className="flex gap-1  justify-center items-center text-neutral-500 tracking-tight ">
           {/* <span className="size-1 rounded-full bg-green-500"></span> */}
-          <div className="flex justify-center items-center mb-[2px]">
-            <MessageCircleCode size={12} />
-          </div>
-          Contact me!
+          Freelance | Full-time
         </div>
       </motion.div>
     </div>
